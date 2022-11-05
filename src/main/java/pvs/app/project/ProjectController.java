c984299434f44a7604b17d3dfcbc79f1f8b48ce1
Joey Wu <yoj845@gmail.com>
2022-11-05 13:40:15 +0800

    :sparkles: updated urls for project

--- src/main/java/pvs/app/project/ProjectController.java
+++ src/main/java/pvs/app/project/ProjectController.java
@@ -8,19 +8,19 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pvs.app.project.get.ResponseProjectDTO;
-import pvs.app.project.post.CreateProjectDTO;
 import pvs.app.project.hyperlink.HyperlinkService;
 import pvs.app.project.hyperlink.post.AddGithubRepositoryHyperlinkDTO;
 import pvs.app.project.hyperlink.post.AddSonarQubeHyperlinkDTO;
+import pvs.app.project.post.CreateProjectDTO;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/projects", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProjectController {
-    
+
     static final Logger logger = LogManager.getLogger(ProjectController.class.getName());
 
     @Value("${message.exception}")
@@ -34,38 +34,37 @@ public class ProjectController {
 
     @Value("${message.fail}")
     private String failMessage;
-    
+
     private final ProjectService projectService;
+
     private final HyperlinkService HyperlinkService;
 
-    public ProjectController(ProjectService projectService, HyperlinkService HyperlinkService){
+    public ProjectController(ProjectService projectService, HyperlinkService HyperlinkService) {
         this.projectService = projectService;
         this.HyperlinkService = HyperlinkService;
     }
 
-    @GetMapping("/repository/github/check")
+    @GetMapping("/hyperlinks/github")
     public ResponseEntity<String> checkGithubURL(@RequestParam("url") String url) {
-        if(HyperlinkService.checkGithubURL(url)) {
+        if (HyperlinkService.checkGithubURL(url)) {
             return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        else {
+        } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
         }
     }
 
-    @GetMapping("/repository/sonar/check")
+    @GetMapping("/hyperlinks/sonarqube")
     public ResponseEntity<String> checkSonarURL(@RequestParam("url") String url) {
-        if(HyperlinkService.checkSonarURL(url)) {
+        if (HyperlinkService.checkSonarURL(url)) {
             return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-        }
-        else {
+        } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
         }
     }
 
-    @PostMapping("/project")
+    @PostMapping("/")
     public ResponseEntity<String> createProject(@RequestBody CreateProjectDTO projectDTO) {
-        try{
+        try {
             projectService.create(projectDTO);
             return ResponseEntity.status(HttpStatus.OK).body(successMessage);
         } catch (IOException e) {
@@ -75,70 +74,54 @@ public class ProjectController {
         }
     }
 
-    @PostMapping("/project/{projectId}/repository/sonar")
+    @PostMapping("/{projectId}/hyperlinks/sonarqube")
     public ResponseEntity<String> addSonarRepository(@RequestBody AddSonarQubeHyperlinkDTO addSonarQubeHyperlinkDTO) {
-        try{
-            if(HyperlinkService.checkSonarURL(addSonarQubeHyperlinkDTO.getRepositoryURL())) {
-                if(projectService.addSonarRepo(addSonarQubeHyperlinkDTO)) {
+        try {
+            if (HyperlinkService.checkSonarURL(addSonarQubeHyperlinkDTO.getRepositoryURL())) {
+                if (projectService.addSonarRepo(addSonarQubeHyperlinkDTO)) {
                     return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-                } else {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
                 }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
             }
-            else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-            }
-        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
+        } catch (Exception e) {
             e.printStackTrace();
             logger.debug(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
         }
     }
 
-    @PostMapping("/project/{projectId}/repository/github")
+    @PostMapping("/{projectId}/hyperlinks/github")
     public ResponseEntity<String> addGithubRepository(@RequestBody AddGithubRepositoryHyperlinkDTO addGithubRepositoryHyperlinkDTO) {
-        try{
-            if(HyperlinkService.checkGithubURL(addGithubRepositoryHyperlinkDTO.getRepositoryURL())) {
-                if(projectService.addGithubRepo(addGithubRepositoryHyperlinkDTO)) {
+        try {
+            if (HyperlinkService.checkGithubURL(addGithubRepositoryHyperlinkDTO.getRepositoryURL())) {
+                if (projectService.addGithubRepo(addGithubRepositoryHyperlinkDTO)) {
                     return ResponseEntity.status(HttpStatus.OK).body(successMessage);
-                } else {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
                 }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failMessage);
             }
-            else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
-            }
-        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(urlInvalidMessage);
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
         }
     }
 
-
-    @GetMapping("/project/{memberId}")
+    @GetMapping("/members/{memberId}")
     public ResponseEntity<List<ResponseProjectDTO>> readMemberAllProjects(@PathVariable Long memberId) {
         List<ResponseProjectDTO> projectList = projectService.getMemberProjects(memberId);
         return ResponseEntity.status(HttpStatus.OK).body(projectList);
-        //-/-/-/-/-/-/-/-/-/-/
-        //    0        0    //
-        //         3        //
-        //////////\\\\\\\\\\\\
     }
 
-    @GetMapping("/project/{memberId}/{projectId}")
+    @GetMapping("/{projectId}/members/{memberId}")
     public ResponseEntity<ResponseProjectDTO> readSelectedProject
             (@PathVariable Long memberId, @PathVariable Long projectId) {
         List<ResponseProjectDTO> projectList = projectService.getMemberProjects(memberId);
         Optional<ResponseProjectDTO> selectedProject =
                 projectList.stream()
-                           .filter(project -> project.getProjectId().equals(projectId))
-                           .findFirst();
+                        .filter(project -> project.getProjectId().equals(projectId))
+                        .findFirst();
 
         return selectedProject.map(responseProjectDTO -> ResponseEntity.status(HttpStatus.OK).body(responseProjectDTO))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null));
-
-        //-/-/-/-/-/-/-/-/-/-/
-        //    0        0    //
-        //         3        //
-        //////////\\\\\\\\\\\\
     }
 }
