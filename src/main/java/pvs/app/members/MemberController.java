a06bb11d2420a965a3f5f7ee4e38a95f9d587327
Joey Wu <yoj845@gmail.com>
2022-11-13 00:07:23 +0800

    :truck: renamed methods in MemberController

--- src/main/java/pvs/app/members/MemberController.java
+++ src/main/java/pvs/app/members/MemberController.java
@@ -1,5 +1,6 @@
 package pvs.app.members;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,6 +26,7 @@ public class MemberController {
     }
 
     @PostMapping("/members")
+    @ApiOperation(value = "新建使用者")
     public ResponseEntity<String> createMember(@RequestBody MemberDTO memberDTO) {
         try {
             if (null != memberService.createUser(memberDTO)) {
@@ -38,18 +40,20 @@ public class MemberController {
     }
 
     @GetMapping("/members/{memberId}/projects")
-    public ResponseEntity<List<ProjectOfResponse>> readMemberAllProjects(@PathVariable Long memberId) {
+    @ApiOperation(value = "取得使用者參與的所有專案")
+    public ResponseEntity<List<ProjectOfResponse>> getAllProjectsByMemberId(@PathVariable Long memberId) {
         List<ProjectOfResponse> projectList = projectService.getProjectsByMember(memberId);
         return ResponseEntity.status(HttpStatus.OK).body(projectList);
     }
 
     @GetMapping("/members/{memberId}/projects/{projectId}")
-    public ResponseEntity<ProjectOfResponse> readSelectedProject(
-            @PathVariable Long memberId, @PathVariable Long projectId) {
+    @ApiOperation(value = "取得使用者參與的指定專案")
+    public ResponseEntity<ProjectOfResponse>
+    getProjectByMemberIdAndProjectId(@PathVariable Long memberId, @PathVariable Long projectId) {
         ProjectOfResponse selectedProject = projectService.getProjectsFromMemberById(projectId, memberId);
         if (selectedProject != null)
             return ResponseEntity.status(HttpStatus.OK).body(selectedProject);
         else
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
