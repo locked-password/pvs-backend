47224db88ad50dd69c1bad34534d4c232c805263
Joey Wu <yoj845@gmail.com>
2022-11-12 22:36:39 +0800

    :recycle: extract exception handler

--- src/main/java/pvs/app/api/sonarqube/SonarQubeAgentController.java
+++ src/main/java/pvs/app/api/sonarqube/SonarQubeAgentController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -32,90 +29,79 @@ public class SonarQubeAgentController {
     }
 
     @GetMapping("/components/{component}/coverage")
-    public ResponseEntity<String> getCoverage(@PathVariable("component") String component) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<CodeCoverageDTO> coverages = sonarQubeAgentService.getSonarCodeCoverage(component);
-            if (!coverages.isEmpty()) {
-                String coverageString = objectMapper.writeValueAsString(coverages);
+    public ResponseEntity<String> getCoverage(@PathVariable("component") String component) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<CodeCoverageDTO> coverages = sonarQubeAgentService.getSonarCodeCoverage(component);
+        if (!coverages.isEmpty()) {
+            String coverageString = objectMapper.writeValueAsString(coverages);
 
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(coverageString);
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(failMessage);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.debug(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(exceptionMessage);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(coverageString);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(failMessage);
         }
     }
 
     @GetMapping("/components/{component}/bug")
-    public ResponseEntity<String> getBug(@PathVariable("component") String component) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<BugDTO> bugList = sonarQubeAgentService.getSonarBug(component);
-            if (!bugList.isEmpty()) {
-                String bugListString = objectMapper.writeValueAsString(bugList);
+    public ResponseEntity<String> getBug(@PathVariable("component") String component) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<BugDTO> bugList = sonarQubeAgentService.getSonarBug(component);
+        if (!bugList.isEmpty()) {
+            String bugListString = objectMapper.writeValueAsString(bugList);
 
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(bugListString);
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(failMessage);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.debug(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(exceptionMessage);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(bugListString);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(failMessage);
         }
     }
 
     @GetMapping("/components/{component}/code_smell")
-    public ResponseEntity<String> getCodeSmell(@PathVariable("component") String component) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<CodeSmellDTO> codeSmellList = sonarQubeAgentService.getSonarCodeSmell(component);
-            if (!codeSmellList.isEmpty()) {
-                String codeSmellListString = objectMapper.writeValueAsString(codeSmellList);
+    public ResponseEntity<String> getCodeSmell(@PathVariable("component") String component) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<CodeSmellDTO> codeSmellList = sonarQubeAgentService.getSonarCodeSmell(component);
+        if (!codeSmellList.isEmpty()) {
+            String codeSmellListString = objectMapper.writeValueAsString(codeSmellList);
 
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(codeSmellListString);
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(failMessage);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.debug(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(exceptionMessage);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(codeSmellListString);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(failMessage);
         }
     }
 
     @GetMapping("/components/{component}/duplication")
-    public ResponseEntity<String> getDuplication(@PathVariable("component") String component) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<DuplicationDTO> duplicationList = sonarQubeAgentService.getDuplication(component);
-            if (!duplicationList.isEmpty()) {
-                String duplicationListString = objectMapper.writeValueAsString(duplicationList);
+    public ResponseEntity<String> getDuplication(@PathVariable("component") String component) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<DuplicationDTO> duplicationList = sonarQubeAgentService.getDuplication(component);
+        if (!duplicationList.isEmpty()) {
+            String duplicationListString = objectMapper.writeValueAsString(duplicationList);
 
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(duplicationListString);
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(failMessage);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.debug(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(exceptionMessage);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(duplicationListString);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(failMessage);
         }
+    }
+
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> ioExceptionHandler(Exception e) {
+        logger.debug(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exceptionMessage);
     }
 }
